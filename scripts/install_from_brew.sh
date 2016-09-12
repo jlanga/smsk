16ee4f9ad7e4ac7c81ba259977d6f525120d76dd
Jorge Langa <jorge.langa.arranz@gmail.com>
2016-09-12 16:04:59 +0200

    Added support to R packages
    Former-commit-id: 31c139f607475e8425d5459e17700d14ae780832

--- scripts/install_from_brew.sh
+++ scripts/install_from_brew.sh
@@ -1,11 +1,21 @@
 brew update
 
+# To make the example work
 brew install \
     python3 \
     graphviz
 
+# Homebrew science packages to run the example
 brew tap homebrew/science
 brew install \
     homebrew/science/bwa \
     homebrew/science/samtools \
     homebrew/science/bcftools
+
+
+# Examples of other languages: python3, R, ruby, perl
+# brew install \
+#     python3 \
+#     homebrew/science/r \
+#     ruby \
+#     perl
