1eaf1515343ea80f62e3559f3a5619d0460cecec
Jorge Langa <jorge.langa.arranz@gmail.com>
2016-12-05 11:47:48 +0100

    Bugfix
    Former-commit-id: ef36b2752df13e095c27d396312dd95b215ad4c9

--- bin/install/conda_env.sh
+++ bin/install/conda_env.sh
@@ -1,1 +1,5 @@
+conda config --add channels conda-forge
+conda config --add channels defaults
+conda config --add channels r
+conda config --add channels bioconda
 conda env create --name smsk --file bin/install/requirements.txt
--- bin/install/from_gem.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-# This is an example. Install ruby. Then use gem
-# brew install \
-#    ruby
-
-# gem install \
-#    --install-dir .gem/ \
-#    rake
