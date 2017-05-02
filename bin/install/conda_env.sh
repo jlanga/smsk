9f2fa5182404560f92f1167baa1b9e6783a30e1e
Jorge Langa <jorge.langa.arranz@gmail.com>
2017-05-02 12:00:53 +0200

    Cleaning up
    Former-commit-id: d025dbdd22f7ac5288c7b1e9cebe539f5d4c3a92

--- bin/install/conda_env.sh
+++
@@ -1,5 +0,0 @@
-conda config --add channels conda-forge
-conda config --add channels defaults
-conda config --add channels r
-conda config --add channels bioconda
-conda env create --name smsk --file bin/install/requirements.txt
