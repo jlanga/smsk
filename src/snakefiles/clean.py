e63712ad4fafc1eabd1524cacf0cb8992ffb43ac
Jorge Langa <jorge.langa.arranz@gmail.com>
2018-06-11 17:13:23 +0200

    Docstrings in clean.py

--- src/snakefiles/clean.py
+++ src/snakefiles/clean.py
@@ -1,18 +1,22 @@
 rule clean_map:
+    """Just remove mappings"""
     shell:
         "rm -rf {MAP}"
 
 
 rule clean_call:
+    """Just remove the SNP calling"""
     shell:
         "rm -rf {CALL}"
 
 
 rule clean_report:
+    """Just remove the report"""
     shell:
         "rm -rf {REPORT_CALL}"
 
 
 rule clean:
+    """Delete everything"""
     shell:
         "rm -rf {MAP} {CALL} {REPORT_CALL}"
