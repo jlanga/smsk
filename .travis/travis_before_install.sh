4b70d789ec0b4e364d6c3c41cbc92cbdda4687ea
Jorge Langa <jorge.langa.arranz@gmail.com>
2018-06-11 18:15:30 +0200

    Linted travis files

--- .travis/travis_before_install.sh
+++ .travis/travis_before_install.sh
@@ -2,21 +2,21 @@
 
 # Install miniconda
 
-if [[ -d $HOME/miniconda3/bin ]]; then
+if [[ -d "$HOME"/miniconda3/bin ]]; then
     echo "miniconda3 already installed."
 else
     echo "Installing miniconda3"
-    mkdir -p $HOME/download
+    mkdir -p "$HOME"/download
     url="https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh"
     wget \
         --continue \
-        --output-document $HOME/download/miniconda.sh \
+        --output-document "$HOME"/download/miniconda.sh \
         $url
-    chmod +x $HOME/download/miniconda.sh
-    $HOME/download/miniconda.sh \
+    chmod +x "$HOME"/download/miniconda.sh
+    "$HOME"/download/miniconda.sh \
         -u \
         -b \
-        -p $HOME/miniconda3
+        -p "$HOME"/miniconda3
 
-    $HOME/miniconda3/bin/conda clean --all --yes
+    "$HOME"/miniconda3/bin/conda clean --all --yes
 fi
