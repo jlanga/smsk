af6534913e2ce7d6371d34322a0f8b4cc6fc0a98
Jorge Langa <jorge.langa.arranz@gmail.com>
2018-11-27 11:39:40 +0100

    Channels in proper order. Cleaning up.

--- src/install/configure_conda.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-conda config --add channels defaults
-conda config --add channels conda-forge
-conda config --add channels bioconda
-conda clean --all --yes
